11e0dc17a1c18def708153b3bcf3fe92788112e8
sravan <57430307+sravan1946@users.noreply.github.com>
2022-10-03 15:33:24 +0530

    add jsk

--- hacksquad_discordpy/main.py
+++ hacksquad_discordpy/main.py
@@ -1,23 +1,23 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 intents = discord.Intents.all()
 
 my_secret = os.environ["Token"]  # Bot Token
 bot = commands.Bot(command_prefix="!", intents=intents)
 activity = discord.Activity(type=discord.ActivityType.listening, name="!help")
 
-bot.remove_command("help")
-
 @bot.event
 async def on_ready():
+    await bot.load_extension("jishaku")
     print("Bot is running as {0.user}".format(bot))
 
-
 @bot.hybrid_command()
-async def help(ctx):
-    await ctx.send("Help")
+async def ping(ctx):
+    await ctx.send("Pong!")
 
 @bot.command()
 async def sync(ctx):
