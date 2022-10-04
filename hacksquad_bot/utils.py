6b11dcd5eec35c471f19ae6bddc5f4c26fa16f14
HellFire <82315185+Artemis6969@users.noreply.github.com>
2022-10-04 22:23:06 +0530

    Contributor functions (#23)
    * Add docstring for commands
    
    * Implement `get_contributors`
    
    * Refactor API helpers
    
    Co-authored-by: sravan <57430307+sravan1946@users.noreply.github.com>
    Co-authored-by: Julien Mauroy <pro.julien.mauroy@gmail.com>

--- hacksquad_bot/utils.py
+++ hacksquad_bot/utils.py
@@ -1,27 +1,43 @@
 import aiohttp
 import asyncio
-import json
+
+class ResponseError(Exception):
+    """Something went wrong with the response"""
+    pass
 
 async def get_leaderboard():
     async with aiohttp.ClientSession() as session:
         async with session.get('https://www.hacksquad.dev/api/leaderboard') as response:
             if response.status == 200:
-                response = await response.text()
-                response = json.loads(response)
-                teams = response.get("teams")
-                return teams
-
+                response = await response.json()
+                return response.get("teams")
             else:
-                return None
+                raise ResponseError
 
 async def get_team(slug):
     async with aiohttp.ClientSession() as session:
         async with session.get(f'https://www.hacksquad.dev/api/team/?id={slug}') as response:
             if response.status == 200:
-                response = await response.text()
-                response = json.loads(response)
-                team = response.get("team")
-                return team
-
+                response = await response.json()
+                return response.get("team")
             else:
-                return None
+                raise ResponseError
+
+async def get_contributors():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://contributors.novu.co/contributors') as response:
+            if response.status == 200:
+                response = await response.json()
+                return response.get("list")
+            else:
+                raise ResponseError
+
+async def get_contributors_mini():
+    # I do not think that we would get much of a performance benefit from this but leaving it here all the same
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://contributors.novu.co/contributors-mini') as response:
+            if response.status == 200:
+                response = await response.json()
+                return response.get("list")
+            else:
+                raise ResponseError
