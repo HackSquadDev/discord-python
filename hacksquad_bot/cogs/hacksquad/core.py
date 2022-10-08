ab564f08215209af075aa776e14155a97f9fc22f
Predeactor <predeactor0@gmail.com>
2022-10-08 23:42:58 +0200

    feat: whatever this add new, it's awesome really

--- hacksquad_bot/cogs/hacksquad/core.py
+++ hacksquad_bot/cogs/hacksquad/core.py
@@ -1,3 +1,4 @@
+import random
 from typing import List, TypedDict
 
 import discord
@@ -6,9 +7,9 @@ from discord.ext import commands
 from tabulate import tabulate
 
 from hacksquad_bot.cogs.hacksquad.utils import Requester
-from hacksquad_bot.main import Context, HackSquadBot
+from hacksquad_bot.main import HackSquadBot
 
-from .utils import HACKSQUAD_COLOR, PRStatus, Requester
+from .utils import HACKSQUAD_COLOR, NovuContributor, NovuPR, PRStatus, Requester
 
 
 class HackSquad(commands.Cog):
@@ -16,18 +17,14 @@ class HackSquad(commands.Cog):
         self.bot = bot
 
     @staticmethod
-    def pr_formatter(pr_data: list):
+    def pr_formatter(pr_data: List[NovuPR]) -> str:
         if len(pr_data) > 3:
             pr_data = pr_data[:3]
-        prs = ""
-        for pr in pr_data:
-            prs += f"[**{pr['title']}**]({pr['url']})\n"
-
-        return prs
+        return "\n".join(f"[**{pr['title']}**]({pr['url']})\n" for pr in pr_data)
 
     @staticmethod
-    def hero_embed_formatter(contributor: dict):
-        name = contributor["name"]
+    def hero_embed_formatter(contributor: NovuContributor) -> discord.Embed:
+        name = contributor["name"] or "Name not found"
         github = f"[{contributor['github']}](https://github.com/{contributor['github']})"
         avatar_url = contributor["avatar_url"]
 
@@ -36,21 +33,25 @@ class HackSquad(commands.Cog):
 
         embed = discord.Embed(
             title=f"{name}",
-            description=f"**Github:** [{github}](https://github.com/{github})\n\n**Number of PRs:** {number_of_pulls}\n**Last 3 PRs:\n**{HackSquad(HackSquadBot).pr_formatter(pulls)}\n",
-            color=discord.Color.random(),
+            description=f"**GitHub:** [{github}](https://github.com/{github})\n\n**Number of PRs:** {number_of_pulls}",
+            color=HACKSQUAD_COLOR,
         )
+        if pulls:
+            embed.add_field(name=f"Last {len(pulls)} PRs", value=HackSquad.pr_formatter(pulls))
         embed.set_thumbnail(url=avatar_url)
 
         return embed
 
-    @commands.hybrid_command()
-    async def leaderboard(self, ctx: Context):
+    @app_commands.command()
+    async def leaderboard(self, interaction: Interaction, *, page: int):
         """
         Show the leaderboard of HackSquad 2022!
         """
         if page <= 0:
             await interaction.response.send_message("The page cannot be negative or 0.")
             return
+        await interaction.response.defer()
+
         page -= 1
 
         class MinimifiedPartialTeam(TypedDict):
@@ -83,7 +84,7 @@ class HackSquad(commands.Cog):
             tablefmt="pretty",
         )
         msg += f"\nShowing {len(teams)} teams - Page {page + 1}/{len(list_of_teams)}"
-        await interaction.response.send_message(f"```diff\n{msg}\n```")
+        await interaction.followup.send(f"```diff\n{msg}\n```")
 
     @app_commands.command()
     async def team(self, interaction: Interaction, *, team_slug: str):
@@ -92,6 +93,8 @@ class HackSquad(commands.Cog):
 
         Use the "Slug" as the team's name.
         """
+        await interaction.response.defer()
+
         results = await Requester().fetch_team(team_slug)
 
         embed = discord.Embed(title=f"Team: {results['name']}", color=HACKSQUAD_COLOR)
@@ -115,44 +118,67 @@ class HackSquad(commands.Cog):
         if prs_str := "\n".join([f"**[{pr['title']}]({pr['url']})**" for pr in last_3_prs]):
             embed.description += f"\n\n**{len(last_3_prs)} last accepted PRs:**\n{prs_str}"
 
-        await interaction.response.send_message(embed=embed)
+        await interaction.followup.send(embed=embed)
 
     @app_commands.command(name="search")
     async def search_team(self, interaction: Interaction, *, query: str):
+        """
+        Try to search for a team in the leaderboard.
+        """
+        if len(query) <= 3:
+            await interaction.response.send_message(
+                "The query needs to be more than 3 characters long."
+            )
+            return
+
+        await interaction.response.defer()
+
         results = await Requester().fetch_leaderboard()
 
         matching = [x for x in results if query in x["name"]]
 
         if not matching:
-            await interaction.response.send_message("No results were found...")
+            await interaction.followup.send("No results were found.")
             return
 
-        list_str = "\n".join(
-            f"- Name: `{match['name']}` / Slug: `{match['slug']}`" for match in matching
+        list_str = "\n\n".join(
+            f"- Name: {match['name']}\n- Slug: {match['slug']}" for match in matching
         )
-        await interaction.response.send_message(f"__Results__:\n\n{list_str}")
+        if len(list_str) > 2000:
+            await interaction.followup.send(
+                "Too much results were found, try to refine your query."
+            )
+            return
+        await interaction.followup.send(
+            f"__Results of {len(matching)} matchs:__\n\n```\n{list_str}\n```"
+        )
 
     @app_commands.command()
     async def hero(self, interaction: discord.Interaction, *, hero: str):
         """
-        Show the details of heros who have contributed to Hacksquad 2022!
+        Show the details of an hero that have contributed to Novu.
         """
-        message = await ctx.send("Proccessing request...")
-        contributors = await Requester().fetch_contributors()
-        for contributor in contributors:
-            contributor = dict(contributor)
-            if contributor["github"] == hero:
-                await message.edit(
-                    content=None, embed=HackSquad(HackSquadBot).hero_embed_formatter(contributor)
-                )
+        await interaction.response.defer()
 
-    @commands.hybrid_command()
-    async def randomhero(self, ctx: Context):
+        contributors = await Requester().fetch_contributors()
+
+        for contributor in contributors:
+            if contributor["github"].lower() == hero.lower():
+                await interaction.followup.send(embed=self.hero_embed_formatter(contributor))
+                return
+
+        await interaction.followup.send(content="Sorry, this hero was not found :( Try again!")
+
+    @app_commands.command()
+    async def randomhero(self, interaction: Interaction):
         """
-        Show the details of heros who have contributed to Hacksquad 2022!
+        Show the details of a random hero who have contributed to Novu.
         """
-        message = await ctx.send("Getting a random hero... That one must be awesome")
+        await interaction.response.defer()
+
         contributors = await Requester().fetch_contributors()
         contributor = random.choice(contributors)
 
-        await message.edit(content=None, embed=self.hero_embed_formatter(contributor))
+        await interaction.followup.send(  # type: ignore
+            content="", embed=self.hero_embed_formatter(contributor)
+        )
